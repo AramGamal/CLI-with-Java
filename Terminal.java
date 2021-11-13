0d4b43710ae56b93b85a058c8030e3387bd2a14b
AramGamal <84885571+AramGamal@users.noreply.github.com>
2021-11-13 22:18:59 +0200

    Add files via upload

--- Terminal.java
+++ Terminal.java
@@ -5,6 +5,12 @@
  */
 package com.mycompany.projectos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
@@ -16,8 +22,8 @@ import java.util.Scanner;
 
 
 class Parser{
-  String commandName;
-  String[] args;
+  public String commandName;
+  public String[] args;
     
     
     public boolean parse(String input){
@@ -32,6 +38,7 @@ class Parser{
             if(i==this.args.length-1){
                 this.args[i]="";
             }
+            
         }
                
        
@@ -52,6 +59,58 @@ class Parser{
 
 public class Terminal {
     
+     Parser p = new Parser();
+     String []message;
+     String path;
+     
+     public  String[] echo(Parser p){
+        return p.getArgs();
+     }
+     
+     public void pwd(){
+        this.path = System.getProperty("user.dir");
+         System.out.println(this.path);
+         
+     }
+     
+     public void ls(){
+       
+       File directory = new File(".");  //current directory
+       File []fileList = directory.listFiles();
+         for (File fileList1 : fileList) {
+             System.out.println(fileList1);
+         }
+
+     }
+     
+     public void lsReverse(){
+         File directory2 = new File(".");  //current directory
+         File []fileList = directory2.listFiles();
+            for (int i=fileList.length-1; i>=0;i--){
+                System.out.println(fileList[i]);
+            }
+         }
+     
+     
+    public static void cat(String[] args)
+{
+    for (int i = 0; i < args.length - 1; i++)
+    {
+        try {
+            FileReader fileReader = new FileReader(args[i]);
+            BufferedReader in = new BufferedReader(fileReader);
+            String line;
+            while((line = in.readLine())!= null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println(args[0]+", file not found.");
+        }
+        catch (IOException ex) {
+            System.out.println(args[0]+", input/output error.");
+        }
+    }
+}
     
     
         
@@ -59,22 +118,51 @@ public class Terminal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+        Terminal t = new Terminal();
         Parser pars = new Parser();
-      
-       
-        pars.parse("dbhd disb sdv");
-        System.out.println(pars.getCommandName());
-        String y[]=pars.getArgs();
-        for (int i=0; i<y.length;i++){
-            
-            System.out.print(y[i]+" ");
-            if(i==y.length-1){
-                System.out.print(y[i]);
-            }
-        }
-       
+        boolean stop = false;
         
-    }
-    
+        
+        Scanner in = new Scanner(System.in);
+        
+       while (stop==false){
+            String input = in.nextLine();
+            pars.parse(input);
+            if("echo".equals(pars.getCommandName())){              //echo command    
+                String [] echoReturn=t.echo(pars);
+            for (String echoReturn1 : echoReturn) {
+                System.out.print(echoReturn1 + " ");
+            }
+                
+            }
+            else if ("pwd".equals(pars.getCommandName())){        //pwd command
+                t.pwd();
+            }
+            else if("cat".equals(pars.getCommandName())){         //cat command
+                t.cat(pars.getArgs());
+            }
+            else if("ls".equals(pars.getCommandName())){          //ls command
+                t.ls();
+                String[]argss = pars.args;
+                
+                   if ("-r".equals(argss[0])){                    //ls -r command
+                       t.lsReverse();
+                       
+                   }
+                
+                
+                
+            }
+ 
+          
+            
+            
+            
+        
+        
+ }
+        
+  }
 }
+
+   
